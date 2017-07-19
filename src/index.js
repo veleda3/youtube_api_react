738800657ee8a47c5589da70414f14af86fed179
Daniel Velez <velez.a.daniel@gmail.com>
2017-07-19 11:23:06 -0400

    working on youtube api react

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTsearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyC7FnbtzSJG0iGTxpRh_nrEQMJn5LGj05A';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component  {
+  constructor(props) {
+    super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('tour de france');
+  }
+
+ videoSearch(term) {
+      YTsearch({ key: API_KEY, term: term}, (data) => {
+          this.setState({
+            videos: data,
+            selectedVideo: data[0]
+          });
+      });
+    }
+
+
+  render () {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 500)
+    return (
+    <div>
+    <SearchBar onSearchTermChange={videoSearch}/>
+    <VideoDetail video={this.state.selectedVideo} />
+    <VideoList
+      onVideoSelect={selectedVideo => this.setState({selectedVideo: selectedVideo})}
+      videos={this.state.videos} />
+    </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
